ec98873e7bd2a946e254a5e03953aca078622b82
shortcutme <tamas@zeronet.io>
2020-11-14 02:05:43 +0100

    Force https handled by server config

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-if (!isset($_SERVER['HTTPS']) or $_SERVER['HTTPS'] == 'off' ) {
-  $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-  header("Location: $redirect_url");
-  exit();
-}
 if (isset($_GET["url"]))
   $locale = $_GET["url"];
 else
